24429796d00a9dc29d04e60b3dc795a79f0d108f
qyang2 <Hbyt5842217>
2018-10-26 18:17:06 +0800

    no finish

---
+++ leetcode/src/main/java/FindPivotIndex.java
@@ -0,0 +1,29 @@
+/**
+ * @Author: yangqi
+ * @Date: 10/26/2018 6:03 PM
+ *
+ * https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1144/
+ */
+public class FindPivotIndex {
+    /**
+     *
+     * The length of nums will be in the range [0, 10000].
+     * Each element nums[i] will be an integer in the range [-1000, 1000].
+     *
+      * @param nums
+     * @return
+     */
+    public int pivotIndex(int[] nums) {
+        if (nums.length > 10000 ) {
+            throw new RuntimeException("The length of nums will be in the range [0, 10000]");
+        }
+
+
+
+        return 1;
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
