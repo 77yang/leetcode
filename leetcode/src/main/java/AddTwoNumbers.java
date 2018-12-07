00b45186da7b10a4f20cf025a265ae856e2efec7
qyang2 <Hbyt5842217>
2018-12-07 18:20:33 +0800

    add some code

---
+++ leetcode/src/main/java/AddTwoNumbers.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ *
+ *
+ *
+ * @Author: yangqi
+ * @Date: 12/7/2018 6:00 PM
+ */
+public class AddTwoNumbers {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
