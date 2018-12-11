721aef37669e6fba1e4ab8e74b82f16ad8500298
qyang2 <Hbyt5842217>
2018-12-11 17:59:22 +0800

    Runtime: 29 ms, faster than 56.84% of Java online submissions for Add Two Numbers.

--- leetcode/src/main/java/AddTwoNumbers.java
+++ leetcode/src/main/java/AddTwoNumbers.java
@@ -1,60 +1,59 @@
-import java.math.BigInteger;
-
 /**
- *
  * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
  * Output: 7 -> 0 -> 8
  * Explanation: 342 + 465 = 807.
+ * <p>
+ * <p>
+ * https://leetcode.com/problems/add-two-numbers/
  *
- *
- *https://leetcode.com/problems/add-two-numbers/
  * @Author: yangqi
  * @Date: 12/7/2018 6:00 PM
  */
 public class AddTwoNumbers {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        char[] chars =  getNumber(l2).add(getNumber(l1)).toString().toCharArray();
-        ListNode node = null;
-        ListNode head = null;
-        for (int i1 = chars.length - 1; i1 >= 0; i1--) {
-            ListNode listNode = new ListNode(Character.getNumericValue(chars[i1]));
-            if (node == null) {
-                head = listNode;
-                node = listNode;
+
+        ListNode headNode = null,
+                newNode = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            carry = sum / 10;
+            int remainder = sum % 10;
+            ListNode node = new ListNode(remainder);
+            if (headNode == null) {
+                headNode = node;
             } else {
-                node.next = listNode;
-                node = node.next;
+                newNode.next = node;
             }
-
+            newNode = node;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
         }
-        return head;
-
+        if (carry > 0) {
+            newNode.next = new ListNode(carry);
+        }
+        return headNode;
     }
 
-    private static BigInteger getNumber(ListNode l1) {
-        String s1 = "";
-        s1 = l1.val + s1;
-        while (l1.next != null) {
-            l1 = l1.next;
-            s1 = l1.val + s1;
-        }
-        return new BigInteger(s1);
-    }
 
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(2);
-        l1.next=new ListNode(4);
-        l1.next.next = new ListNode(3);
+        ListNode l1 = new ListNode(5);
+/*        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);*/
 
 
         ListNode l2 = new ListNode(5);
-        l2.next=new ListNode(6);
-        l2.next.next = new ListNode(4);
+  /*      l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);*/
 
 
         ListNode listNode = new AddTwoNumbers().addTwoNumbers(l1, l2);
-        System.out.println(Integer.MAX_VALUE);
+        System.out.println(listNode);
 
     }
 }
@@ -63,7 +62,16 @@ public class AddTwoNumbers {
 class ListNode {
     int val;
     ListNode next;
+
     ListNode(int x) {
         val = x;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
     }
 }
