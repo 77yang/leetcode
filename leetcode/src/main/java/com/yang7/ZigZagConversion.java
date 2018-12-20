ca5473dbb3e1d85a6fa0784b0860b46ef888063d
501294009@qq.com <yq5842217>
2018-12-20 23:11:15 +0800

    34 ms

--- leetcode/src/main/java/com/yang7/ZigZagConversion.java
+++ leetcode/src/main/java/com/yang7/ZigZagConversion.java
@@ -1,6 +1,7 @@
 package com.yang7;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Input: s = "PAYPALISHIRING", numRows = 4
@@ -15,43 +16,36 @@ import java.util.*;
 public class ZigZagConversion {
 
     public String convert(String s, int numRows) {
+
+        if(numRows==1) return s;
+
         boolean addOrreduce = true;
 
-        Map<Integer, StringBuilder> data = new HashMap<>();
-        int row = 1;
+        List<StringBuilder> ls = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            ls.add(new StringBuilder());
+        }
+
+        int row = 0;
         int i = 0;
         int length = s.length();
+        int maxIndex = numRows - 1;
+
         while (i < length) {
-            StringBuilder str = data.get(row);
-            if (str == null) {
-                str = new StringBuilder();
-            }
-            str.append(s.charAt(i++));
-
-            data.put(row, str);
-
-            if (row == numRows) {
-                addOrreduce = false;
-            }
-
-            if (row == 1) {
-                addOrreduce = true;
-            }
-
+            ls.get(row).append(s.charAt(i++));
             if (addOrreduce) {
-                row++;
+                if (++row == maxIndex) {
+                    addOrreduce = false;
+                }
             } else {
-                row--;
+                if (--row == 0) {
+                    addOrreduce = true;
+                }
             }
         }
 
-        StringBuffer sb = new StringBuffer();
-
-        data.values().forEach(ls ->
-                sb.append(ls)
-        );
-
-        return sb.toString();
+        return String.join("", ls);
     }
 
 
