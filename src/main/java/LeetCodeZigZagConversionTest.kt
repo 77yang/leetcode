c5fcce5ec96cf3b7900a9d373bdf43144725e018
qyang2 <Hbyt5842217>
2018-12-24 17:55:06 +0800

    un finish  code

--- src/main/java/LeetCodeZigZagConversionTest.kt
+++ src/main/java/LeetCodeZigZagConversionTest.kt
@@ -53,6 +53,14 @@ class LeetCodeZigZagConversionTest {
     }
 
 
+    @Test
+    fun regularExpressionMatching() {
+        println(RegularExpressionMatching.isMatch("mississippi", "mis*is*p*."))
+
+
+    }
+
+
 
 
 }
