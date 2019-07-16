6715d956e219b5caf837f95056c6d758fb06d1dc
qyang2 <Test12345678>
2019-07-16 21:28:11 +0800

    v1

---
+++ src/main/java/com/yang7/3SumClosest.kt
@@ -0,0 +1,18 @@
+package com.yang7
+
+
+/**
+ * https://leetcode.com/problems/3sum-closest/
+ *
+ *
+ *
+Given array nums = [-1, 2, 1, -4], and target = 1.
+
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
+fun threeSumClosestV1(nums: IntArray, target: Int): Int {
+
+
+
+    return 1
+}
